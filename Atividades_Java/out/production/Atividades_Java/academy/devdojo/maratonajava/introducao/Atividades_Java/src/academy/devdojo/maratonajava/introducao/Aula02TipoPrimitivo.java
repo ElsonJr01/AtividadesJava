672dfa3a9d15ea048c24af3b3fa7a13f1dab0753
Elson Junior. <167557011+ElsonJr01@users.noreply.github.com>
2025-03-05 15:49:01 -0300

    Add files via upload

---
+++ Atividades_Java/out/production/Atividades_Java/academy/devdojo/maratonajava/introducao/Atividades_Java/src/academy/devdojo/maratonajava/introducao/Aula02TipoPrimitivo.java
@@ -0,0 +1,26 @@
+package academy.devdojo.maratonajava.introducao;
+
+/**
+ * Comentario javadoc <b>Comentario importante</b>
+ */
+public class Aula02TipoPrimitivo {
+    public static void main(String[] args) {
+        //Tipos primitivos: int, double, float, char, byte, short, long, boolean
+        int idade = 10;
+        long numeroGrande = 10000;
+        char sexo = 'M';
+        byte idadeByte = 10;
+        short idadeShort = 10;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        double salarioDouble = 2000;
+        float salarioFloat = 2500;
+        System.out.println(idade);
+        System.out.println(idade + 10);
+        System.out.println("idade");
+        System.out.println(idadeByte);
+        System.out.println(sexo);
+        System.out.println("O sexo dele e: " + sexo + " Mas ele não é grande em altura");
+        System.out.println("E a idade dele e" + idade);
+    }
+}
