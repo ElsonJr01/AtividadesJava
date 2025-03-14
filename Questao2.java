9bcda6b2e6dfbcc48e57ebc4da93a9ff5d676e4e
Elson Junior. <167557011+ElsonJr01@users.noreply.github.com>
2025-03-14 11:44:26 -0300

    Add files via upload

---
+++ Questao2.java
@@ -0,0 +1,13 @@
+public class Questao2 {
+    public static void main(String[] args) {
+         int numero = 25;
+         double preco = 19.99;
+         boolean estaChovendo = false;
+         char letra = 'A';
+
+         System.out.println("Valor do inteiro: " + numero);
+         System.out.println("Valor do Double: " + preco);
+         System.out.println("Valor do Boolean: " + estaChovendo);
+         System.out.println("Valor da letra: " + letra);
+    }
+}
