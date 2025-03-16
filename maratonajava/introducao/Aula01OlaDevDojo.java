2d7d900a24e3298ff578821d4aa55ff86385c976
Elson Junior. <167557011+ElsonJr01@users.noreply.github.com>
2025-03-16 13:14:02 -0300

    Add files via upload

---
+++ maratonajava/introducao/Aula01OlaDevDojo.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratonajava.introducao;
+/**
+ * isso é um <b>comentario javadoc como em html</b>
+ */
+public class Aula01OlaDevDojo {
+    /**
+     * isso é um <b>comentario javadoc parecido com html</b>
+     */
+    public static void main(String[] args) {
+        System.out.println("Estou aprendendo java");
+        System.out.println("Java");
+       // System.out.println("Hora de inicio");
+
+        //CTRL + B e CTRL + Q  ambos, encima de qualquer função mostra pra que serve e como usar
+    }
+}
