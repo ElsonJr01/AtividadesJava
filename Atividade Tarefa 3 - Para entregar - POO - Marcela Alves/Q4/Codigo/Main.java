ca89c9f11d81f10aa7705df5d0756f511e01b794
Elson Junior. <167557011+ElsonJr01@users.noreply.github.com>
2025-05-18 21:36:15 -0300

    Update Main.java

--- Atividade Tarefa 3 - Para entregar - POO - Marcela Alves/Q4/Codigo/Main.java
+++ Atividade Tarefa 3 - Para entregar - POO - Marcela Alves/Q4/Codigo/Main.java
@@ -6,36 +6,30 @@ public class Main {
     public static void main(String[] args) {
         Hospital hospital = new Hospital("Hospital Central");
 
-        // Criando unidades
-        Enfermaria enfermaria1 = new Enfermaria("Enfermaria Geral");
-        Enfermaria prontoSocorro = new Enfermaria("Pronto Socorro");
+        
+                        Enfermaria enfermaria1 = new Enfermaria("Enfermaria Geral");
+                        Enfermaria prontoSocorro = new Enfermaria("Pronto Socorro");
+                
+                        
+                        hospital.adicionarUnidade(enfermaria1);
+                        hospital.adicionarUnidade(prontoSocorro);
+                
+                      
+                        Medico medico1 = new Medico("Dr. Elson");
+                        Medico medico2 = new Medico("Dra. MArgarida");
+                
+                        
+                            enfermaria1.adicionarMedico(medico1);
+                            prontoSocorro.adicionarMedico(medico2);
 
-        // Adicionando unidades ao hospital
-        hospital.adicionarUnidade(enfermaria1);
-        hospital.adicionarUnidade(prontoSocorro);
+                        Paciente paciente1 = new Paciente("Carlos Silva", "123.456.789-10");
+                
+                        hospital.adicionarPaciente(paciente1);
 
-        // Criando médicos
-        Medico medico1 = new Medico("Dr. Elson");
-        Medico medico2 = new Medico("Dra. MArgarida");
+                            Consulta consulta1 = new Consulta(LocalDate.now(), "Consulta de rotina", paciente1, medico1);
+                        
+                            medico1.adicionarConsulta(consulta1);
 
-        // Adicionando médicos às unidades
-        enfermaria1.adicionarMedico(medico1);
-        prontoSocorro.adicionarMedico(medico2);
-
-        // Criando pacientes
-        Paciente paciente1 = new Paciente("Carlos Silva", "123.456.789-10");
-
-        // Adicionando paciente ao hospital
-        hospital.adicionarPaciente(paciente1);
-
-        // Criando consulta
-        Consulta consulta1 = new Consulta(LocalDate.now(), "Consulta de rotina", paciente1, medico1);
-
-
-        // Associando consulta ao médico
-        medico1.adicionarConsulta(consulta1);
-
-        // Exibindo resumo da consulta
         System.out.println("\nResumo da consulta:");
         consulta1.getResumo();
     }
