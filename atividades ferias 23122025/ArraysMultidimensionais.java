73d2c9f985434d2f05c3a7bc6903d12f765f397c
ElsonJr01 <jhule.vaz@gmail.com>
2025-12-23 21:38:21 -0300

    atualização dos arrays fotechs

--- atividades ferias 23122025/ArraysMultidimensionais.java
+++ atividades ferias 23122025/ArraysMultidimensionais.java
@@ -2,8 +2,15 @@ public class ArraysMultidimensionais {
 
     public static void main(String[] args) {
         int[] numeros2 = {1, 2, 3, 4, 5};
-        for (int i = 0; i < numeros2.length; i++) {
-            System.out.println(numeros2[i]);
+        int[] numeros3 = new int[]{1, 2, 3, 4, 5};
+//        for (int i = 0; i < numeros2.length; i++) {
+//            System.out.println(numeros2[i]);
+//        }
+//        for (int i=0; i<numeros3.length; i++){
+//            System.out.println(numeros3[i]);
+//        }
+        for(int nume: numeros2){
+            System.out.println(nume);
         }
     }
 }
