289a891f7969b43baaa60875fd9bd4e431e4c02a
ElsonJr01 <jhule.vaz@gmail.com>
2025-12-23 21:14:11 -0300

    atualização

---
+++ atividades ferias 23122025/ArraysMultidimensionais.java
@@ -0,0 +1,9 @@
+public class ArraysMultidimensionais {
+
+    public static void main(String[] args) {
+        int[] numeros2 = {1, 2, 3, 4, 5};
+        for (int i = 0; i < numeros2.length; i++) {
+            System.out.println(numeros2[i]);
+        }
+    }
+}
